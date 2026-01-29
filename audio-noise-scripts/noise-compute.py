5313ea028b630530956483aaa5db13d9ca9619c6
JT McGuire <jtm209@gmail.com>
2026-01-28 22:56:30 -0600

    clarity update

--- audio-noise-scripts/noise-compute.py
+++ audio-noise-scripts/noise-compute.py
@@ -36,17 +36,21 @@ def dBuA(x_rms):
     return 20*np.log10(x_rms/0.775) - 2
 
 # Convert dBu to Vrms with A-weighting correction
-def vrms(x_dBuA):
+def vrmsA(x_dBuA):
     return 10**((x_dBuA - 0.214)/20)
 
+# Convert dBu to Vrms unweighted
+def vrmsU(x_dBuA):
+    return 10**((x_dBuA - 2.214)/20)
+
 # Combine two uncorrelated white noise sources
-def combine_white_noise(n1, n2):
-    return np.sqrt(n1**2 + n2**2)
+def combine_white_noise(vn1, vn2):
+    return np.sqrt(vn1**2 + vn2**2)
 
 # Compute Vrms of amplifier alone with datasheet-specified
 #   test resistor thermal noise removed
 def vrms_amp_alone(ein, r_test):
-    return np.sqrt(vrms(ein)**2 - tnoise(r_test)**2) 
+    return np.sqrt(vrmsA(ein)**2 - tnoise(r_test)**2) 
 
 # Remove 150 ohm test resistor thermal noise from datasheet EIN figures
 ein0 = dBuA(vrms_amp_alone(ein0, 150))
@@ -58,8 +62,8 @@ print("EIN amp0 short: {:.1f} dBuA".format(ein0))
 print("EIN amp1 short: {:.1f} dBuA".format(ein1))
 
 # Compute EIN with 350 Ohm source impedance
-mic_ein0 = dBuA(combine_white_noise(tnoise(mic_impedance), vrms(ein0)))
-mic_ein1 = dBuA(combine_white_noise(tnoise(mic_impedance), vrms(ein1)))
+mic_ein0 = dBuA(combine_white_noise(tnoise(mic_impedance), vrmsA(ein0)))
+mic_ein1 = dBuA(combine_white_noise(tnoise(mic_impedance), vrmsA(ein1)))
 
 # Print results
 print()
